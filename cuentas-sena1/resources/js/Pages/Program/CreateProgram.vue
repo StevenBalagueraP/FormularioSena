df5817e3bc3bbdf80dc24232d7c0352bcce22b69
chrisfalla <christoferfallavillarreal@gmail.com>
2025-03-31 19:45:11 -0500

    changes names of files to english

---
+++ cuentas-sena1/resources/js/Pages/Program/CreateProgram.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Crear Programa</h1>
+        <!-- Contenido de la vista -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CrearPrograma'
+}
+</script>
