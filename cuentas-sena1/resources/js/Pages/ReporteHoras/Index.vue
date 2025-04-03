7a2a06c978283a365a1aa23e5449afd9b3e1d2ea
KevinDuarte12 <100857261+KevinDuarte12@users.noreply.github.com>
2025-04-02 19:50:56 -0500

    Se eliminaron estilos de ejemplo
    Se eliminaron estilos de ejemplo para comprobar si estaba renderizando el index

--- cuentas-sena1/resources/js/Pages/ReporteHoras/Index.vue
+++ cuentas-sena1/resources/js/Pages/ReporteHoras/Index.vue
@@ -10,13 +10,6 @@
 /*css*/
 h1{
     text-align: center;
-    font-size: 32px;
-    text-decoration: double;
-}
-h1:hover{
-    transition: 1.5s all ease-in;
-    font-size: 48px;
-    text-shadow: 2px 2px 5px red;
-    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    font-size: 24px;
 }
 </style>
